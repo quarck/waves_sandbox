0f14601a4eee06656a56de2d144814eb8c4535c7
Sergey Parshin <quarck@gmail.com>
2022-02-08 02:51:29 +0000

    Lensegit add waves/!

--- waves/waves/World.h
+++ waves/waves/World.h
@@ -32,7 +32,8 @@ namespace waves
 	public:
 		using TMedium = Medium<512, 512, 1>;
 
-		static constexpr double VEL_FACTOR = 0.40; // dV = -k*x/m * dT, this is k*dT/m
+		static constexpr double VEL_FACTOR1 = 0.40; // dV = -k*x/m * dT, this is k*dT/m
+		static constexpr double VEL_FACTOR2 = 0.10; // dV = -k*x/m * dT, this is k*dT/m
 		static constexpr double LOC_FACTOR = 0.1; // dX = V * dT, this is dT
 
 	private:
@@ -45,10 +46,10 @@ namespace waves
 	public:
         World()
         {	
-			for (int i = 256-20; i < 256+20; ++i)
-				for (int j = 256-4; j < 256+4; ++j)
+			for (int i = 40-5; i < 40+5; ++i)
+				for (int j = 256-5; j < 256+5; ++j)
 				{
-					_location.at(i, j, 0) = 5000;
+					_location.at(i, j, 0) = 5000 * 4;
 				}
 		}
 
@@ -95,7 +96,10 @@ namespace waves
 
 						const auto delta_x = _location.data[offset] - neight_average; // displacement relative to the current neightbour average 
 
-						_velocity.data[offset] -= VEL_FACTOR * delta_x;
+						if ((std::pow(x - 200, 2.0) + std::pow(y-256.0, 2.0)) < 50.0 * 50.0)
+							_velocity.data[offset] -= VEL_FACTOR2 * delta_x;
+						else 
+							_velocity.data[offset] -= VEL_FACTOR1 * delta_x;
 
 						offset += y_delta;
 					}
@@ -118,6 +122,19 @@ namespace waves
 				}
 			}
 
+			// prevent the reflections from the edges 
+
+			for (int x = 0; x < TMedium::width(); ++x)
+			{
+				_location.at(x, 0, 0) = _location.at(x, 1, 0);
+				_location.at(x, TMedium::height() - 1, 0) = _location.at(x, TMedium::height() - 2, 0);
+			}
+
+			for (int y = 0; y < TMedium::height(); ++y)
+			{
+				_location.at(0, y, 0) = _location.at(1, y, 0);
+				_location.at(TMedium::width() - 1, y, 0) = _location.at(TMedium::width() - 2, y, 0);
+			}
 
 			_iteration++;
 			return true;
